dd4dca641994fd6cf086557771fc7b0395c6777e
Remigijus P <remziukas@gmail.com>
2012-10-23 16:52:12 +0300

    Sulietuvinau komentarus, pakoregavau išvedimą

--- main.cpp
+++ main.cpp
@@ -7,28 +7,31 @@ int main()
 {
     cout << "Monte Carlo realizacijos pradzia" << endl;
     cout << "(RANDOM SEARCH METHOD)" << endl;
-    // Kintamuju apsirasymas
+    // Kintamųjų apsirašymas
     double f, x; // Funkcijos reiksmes ir argumentas x
     int n; // Uzedavinio dimensija
     double xSprendinys; // Argumento reiksme
-    int fSprendinys; // Uzdavinio sprendinys
+    double fSprendinys; // Uzdavinio sprendinys
 
-    // Vektoriu kurimas, kuriu elementai yra atitiktiniai realus skaiciai
+    // Vektoriu kurimas, kuriu elementai yra atsitiktiniai realus skaiciai
 
     double virsutinis_rezis, apatinis_rezis; // apsirasome intervalo rezius
 
-    cout << "Iveskite inervalo rerzius:" << endl;
+    cout << "Įveskite intervalo rėžius:" << endl;
     cin >> apatinis_rezis >> virsutinis_rezis;
-    cout << "Iveskite vektoriaus ilgi:" << endl;
+    cout << "Įveskite vektoriaus ilgį:" << endl;
     cin >> n;
 
     double vektorius[n];
-
     for(int i=0; i<n; i++)
     {
         vektorius[i] = rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
-        cout << i+1  << ". " << vektorius[i] << endl; // Vektoriaus isvedimas i ekrana
     }
-
+	cout << "x = (";
+	for(int i = 0; i < n; ++i)
+	{
+		cout << vektorius[i] << " "; // Vektoriaus isvedimas i ekrana
+	}
+	cout << ")" << endl;
     return 0;
 }
